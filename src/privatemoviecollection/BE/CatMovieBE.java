dfef9d19c3248a4062f0bb93e06bdc4fb4379e6f
raskyerasken <32293919+raskyerasken@users.noreply.github.com>
2018-01-08 12:45:43 +0100

    hello

---
+++ src/privatemoviecollection/BE/CatMovieBE.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privatemoviecollection.BE;
+
+/**
+ *
+ * @author kasper
+ */
+public class CatMovieBE {
+    
+    
+        private String catId;
+
+    public String getCatId() {
+        return catId;
+    }
+
+    public void setCatId(String catId) {
+        this.catId = catId;
+    }
+
+    
+        private String CategoryId;
+
+    public String getCategoryId() {
+        return CategoryId;
+    }
+
+    public void setCategoryId(String CategoryId) {
+        this.CategoryId = CategoryId;
+    }
+
+    
+    
+        private String movieId;
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+}
--- src/privatemoviecollection/BE/PrivateMovieCollection.java
+++ src/privatemoviecollection/BE/PrivateMovieCollection.java
@@ -11,5 +11,50 @@ package privatemoviecollection.BE;
  */
 public class PrivateMovieCollection 
 {
+ 
+        private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+        private String id;
+
+    
+    public String getId() {
+        return id;
+    }
+
+  
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    
+    
+        private String rating;
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+        private String lastview;
+
+    public String getLastview() {
+        return lastview;
+    }
+
+    public void setLastview(String lastview) {
+        this.lastview = lastview;
+    }
+
     
 }
