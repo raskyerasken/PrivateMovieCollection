274b512a29740b57c8aedb3dbc1ec13f77ed6a79
raskyerasken <32293919+raskyerasken@users.noreply.github.com>
2018-01-08 15:22:34 +0100

    please push

--- src/privatemoviecollection/DAL/PrivateMovieCollectionDAL.java
+++ src/privatemoviecollection/DAL/PrivateMovieCollectionDAL.java
@@ -132,7 +132,35 @@ public class PrivateMovieCollectionDAL
         }    
     }
 
-    
+    public void update(PrivateMovieCollection PrivateMovieCollection) throws SQLServerException, SQLException {
+        try (Connection con = cm.getConnection()) {
+            String sql 
+                    = "UPDATE Movie SET "
+                    + "id=?, name=?, Rating=?, filelink=?, lastview=?) "
+                    + "WHERE id=?";
+            
+            
+            PreparedStatement pstmt 
+                    = con.prepareStatement(sql);
+            pstmt.setString(1, PrivateMovieCollection.getTitle());
+            pstmt.setInt(2, PrivateMovieCollection.getId());
+            pstmt.setInt(3, PrivateMovieCollection.getRating());
+            pstmt.setString(4, PrivateMovieCollection.getFilelink());
+            pstmt.setString(5, PrivateMovieCollection.getLastview());
+            
+            int affected = pstmt.executeUpdate();
+            if (affected<1)
+                throw new SQLException("Movie list could not be updated");
+            
+            
+        }
+        
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(PrivateMovieCollectionDAL.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+    }
     
     
    }
