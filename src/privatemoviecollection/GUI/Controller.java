ed91119481a2f664a9deebdb622d67a07f94f4b2
raskyerasken <32293919+raskyerasken@users.noreply.github.com>
2018-01-09 14:35:55 +0100

    oh god no

--- src/privatemoviecollection/GUI/Controller.java
+++ src/privatemoviecollection/GUI/Controller.java
@@ -5,17 +5,26 @@
  */
 package privatemoviecollection.GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -27,6 +36,10 @@ public class Controller implements Initializable
     private ComboBox<String> selectGenre;
     private ObservableList<String> allListBox;
     private ListView<String> listBox;
+    private Stage primaryStage;
+    @FXML
+    private Button addMovieBtn;
+    
     
 //    ObservableList<String> selectGenre = 
 //    FXCollections.observableArrayList(
@@ -46,4 +59,41 @@ public class Controller implements Initializable
         //selectGenre.getSelectionModel().select("Triangle");
     }    
     
+    
+    private void loadStage(String viewName) throws IOException
+    {
+        primaryStage = (Stage) addMovieBtn.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mytunes/gui/view/" + viewName));
+        Parent root = loader.load();
+
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(root));
+
+        newStage.initModality(Modality.WINDOW_MODAL);
+        newStage.initOwner(primaryStage);
+
+        newStage.show();
+    }
+    
+     private void addMovie()
+    {
+        try
+        {
+            loadStage("addMovie.fxml");
+        }
+        catch (IOException ex)
+        {
+            showErrorDialog("I/O Exception", "DATASTREAM FAILED!", "The requested view could not be loaded.");
+        }
+    }
+     
+     private void showErrorDialog(String title, String header, String message)
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
 }
