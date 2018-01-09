478639aa92a84556b7cd77611f6f853b9e9f3be0
raskyerasken <32293919+raskyerasken@users.noreply.github.com>
2018-01-09 12:28:45 +0100

    new Scenebuilder shiz

--- src/privatemoviecollection/GUI/PrivateMovieCollectionController.java
+++ src/privatemoviecollection/GUI/PrivateMovieCollectionController.java
@@ -10,6 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -17,6 +20,45 @@ import javafx.fxml.Initializable;
  * @author kasper
  */
 public class PrivateMovieCollectionController implements Initializable {
+
+    @FXML
+    private Button saveBtn;
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private TextField movieTitle;
+    @FXML
+    private RadioButton ratingSix;
+    @FXML
+    private RadioButton ratingOne;
+    @FXML
+    private RadioButton ratingTwo;
+    @FXML
+    private RadioButton ratingThree;
+    @FXML
+    private RadioButton ratingFour;
+    @FXML
+    private RadioButton ratingFive;
+    @FXML
+    private RadioButton selectedDrama;
+    @FXML
+    private RadioButton selectedHorror;
+    @FXML
+    private RadioButton selectedAction;
+    @FXML
+    private RadioButton selectedCrime;
+    @FXML
+    private RadioButton selectedComedy;
+    @FXML
+    private RadioButton selectedWar;
+    @FXML
+    private RadioButton selectedThriller;
+    @FXML
+    private RadioButton selectedNoir;
+    @FXML
+    private RadioButton selectedAnimation;
+    @FXML
+    private RadioButton selectedWestern;
 
     /**
      * Initializes the controller class.
@@ -26,8 +68,5 @@ public class PrivateMovieCollectionController implements Initializable {
         // TODO
     }    
 
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-    }
     
 }
