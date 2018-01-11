d10b70e63a7db2a4f24213ecaf889389c9aa5952
raskyerasken <32293919+raskyerasken@users.noreply.github.com>
2018-01-11 17:55:00 +0100

    ijohinh9ouibpiuhuyipj

--- src/privatemoviecollection/GUI/Controller.java
+++ src/privatemoviecollection/GUI/Controller.java
@@ -5,7 +5,6 @@
  */
 package privatemoviecollection.GUI;
 
-import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
@@ -14,17 +13,13 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import static java.util.Collections.list;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -36,16 +31,12 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import privatemoviecollection.BE.CategoryID;
@@ -245,11 +236,7 @@ public class Controller implements Initializable
         return date;
         
     }
-    
-    private Date lastViewDate()
-    {
-        
-    }
+  
     
     @FXML
     private void handleAbout(ActionEvent event) {  //sets the "About Us"
